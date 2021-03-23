a9ba55400901850b4d9f0c88a1297381a491ccbc
space_sti <ghost1wolfy@gmail.com>
2021-03-24 01:08:00 +0700

    Dedicated main controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('testHomePage');
-});
+Route::get('/', [MainController::class, 'index'])->name('site-index');
+
 Route::get('/contacts', function () {
     return view('contacts');
 });
