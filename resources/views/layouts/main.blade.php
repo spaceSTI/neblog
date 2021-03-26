823371a2cf1a0402cd18d03f3f5e041168fa89d4
space_sti <ghost1wolfy@gmail.com>
2021-03-27 00:23:51 +0700

    Responsive markup v1.1 (adapted for mobile)

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,24 +16,35 @@
     @include('flashMessage')
     <!--Header-->
         <div class="row">
-            <header class="col-lg-12 d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-info border-bottom shadow-sm">
-                <p class="col-4 h3 my-0 me-md-auto fw-normal">Neblog</p>
-                <nav class="col-md-4 d-none d-sm-none d-md-block col-6 my-2 my-md-0 me-md-3">
+            <header class="col-lg-12 d-flex flex-column flex-md-row align-items-center pr-1 pl-1 p-3 px-md-4 bg-info border-bottom shadow-sm">
+                <p class="d-none d-sm-none d-md-block col-4 h3 my-0 me-md-auto fw-normal">Neblog</p>
+                <nav class="d-none d-sm-none d-md-block col-md-4  col-6 my-2 my-md-0 me-md-3">
                     <a class="p-2 text-dark" href="/">Блог</a>
                     <a class="p-2 text-dark" href="/contacts">Контакты</a>
                     <a class="p-2 text-dark" href="/about">О сайте</a>
                 </nav>
-                <form class="mb-1 mt-1 col-sm-12 col-md-4 col-lg-4 col-xl-4" action="{{ route('search-form') }}" method="get">
+                {{----}}
+                <form class="d-none d-sm-none d-md-block mb-1 mt-1 col-sm-12 col-md-4 col-lg-4 col-xl-4" action="{{ route('search-form') }}" method="get">
                     <input type="text" name="search" id="search" placeholder="Поиск по сайту" class="form-control">
                 </form>
-                <div class="dropdown">
-                    <button type="button" class="col-12 d-md-none d-lg-none d-xl-none btn btn-warning" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Навигация по сайту
-                    </button>
-                    <div class="dropdown-menu col-12">
-                        <a class="dropdown-item" href="/">Блог</a>
-                        <a class="dropdown-item" href="/contacts">Контакты</a>
-                        <a class="dropdown-item" href="/about">О сайте</a>
+
+                {{--Navigation panel for mobile ver--}}
+                <div class="row col-12 align-items-center pr-1 pl-1">
+                    <p class="col-2 d-md-none d-lg-none d-xl-none h5 my-0 me-md-auto fw-normal pr-1 pl-1">NB</p>
+                    <form class="col-8  d-md-none d-lg-none d-xl-none col-xs-8 mb-1 mt-1 pr-1 pl-1" action="{{ route('search-form') }}" method="get">
+                        <input type="text" name="search" id="search" placeholder="Че ищем?" class="form-control">
+                    </form>
+                    <div class="btn-group dropleft col-2 d-md-none d-lg-none d-xl-none">
+                        <!-- Кнопка -->
+                        <button type="button" class="btn pl-1 pr-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fa fa-bars"></i>
+                        </button>
+                        <!-- Меню -->
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="/">Блог</a>
+                            <a class="dropdown-item" href="/contacts">Контакты</a>
+                            <a class="dropdown-item" href="/about">О сайте</a>
+                        </div>
                     </div>
                 </div>
             </header>
