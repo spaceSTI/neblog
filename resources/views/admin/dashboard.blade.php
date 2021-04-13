c981e6134e4901834d30cf7fd8e5c5f3c330b303
space_sti <ghost1wolfy@gmail.com>
2021-04-13 20:29:33 +0700

    Dashboard v.1  (maybe final ver)

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,20 @@
-<h3>Dashboard</h3>
+<h2>Dashboard</h2>
 
 <ul>
     <li><a href="{{ route('article-form') }}">Add article</a></li>
+</ul>
+
+<ul>
+    <li><a href="{{ route('site-index') }}">Article list</a></li>
+</ul>
+
+<ul>
+    <li><a class="btn btn-dark" href="{{ route('logout') }}"
+            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            {{ __('Logout') }}
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
+    </li>
 </ul>
